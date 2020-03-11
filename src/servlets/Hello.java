57458b7e06fe78cdc0dc5dd3f99a1645e3466b73
wwwcaoyan <2639661607@qq.com>
2020-03-11 14:30:48 +0800

    lecture materials update

--- src/servlets/Hello.java
+++ src/servlets/Hello.java
@@ -12,7 +12,7 @@ public class Hello extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out= resp.getWriter();
-        out.println("Hello World! 好好爱这个世界");
+        out.println("Hello World! ");
     }
 }
 
